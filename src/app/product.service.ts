d87bfaac33bb2051a0031456c4882f99a6fa4ee4
mahadanso <49422390+mahadanso@users.noreply.github.com>
2019-11-04 10:45:14 +0000

    completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,8 @@ import { Http, Response } from '@angular/http';
 
 import { Album } from './album';
 
+import { Product } from './product';
+
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
@@ -11,11 +13,16 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http:Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album> response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
   }
 
 }
